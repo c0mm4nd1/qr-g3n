0509659ac5de4622327c9c576507be4b6de71fbd
c0mm4nd1 <111929298+c0mm4nd1@users.noreply.github.com>
2025-07-18 10:33:28 -0500

    install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,5 @@ echo "⬇️ Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "🎉 Installation complete. \r\n"
+echo "🎉 Installation complete."
 
-echo "To use the tool: \r\n"
-
-echo "#source venv/bin/activate \r\n"
-echo "#python3 qr_g3n.py \r\n"
