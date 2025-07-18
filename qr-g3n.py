76fe8c538188f28dfc4d856719f9fd845e268591
c0mm4nd1 <111929298+c0mm4nd1@users.noreply.github.com>
2025-07-18 10:49:21 -0500

    qr-g3n.py

--- qr-g3n.py
+++ qr-g3n.py
@@ -6,19 +6,19 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 def show_banner():
-    print(Fore.CYAN + Style.BRIGHT + """
-                                .d8888b.           
-                               d88P  Y88b          
-                                    .d88P          
- .d88b.  888d888       .d88b.      8888"  88888b.  
-d88P"88b 888P"        d88P"88b      "Y8b. 888 "88b 
-888  888 888   888888 888  888 888    888 888  888 
-Y88b 888 888          Y88b 888 Y88b  d88P 888  888 
- "Y88888 888           "Y88888  "Y8888P"  888  888 
-     888                   888                     
-Y8b d88P              Y8b d88P                     
- "Y88P"                "Y88P"                      
-           
+    print(Fore.RED + Style.BRIGHT + """
+
+  ░██████   ░█████████                      ░██████             
+ ░██   ░██  ░██     ░██                    ░██   ░██            
+░██     ░██ ░██     ░██          ░████████       ░██ ░████████  
+░██     ░██ ░█████████  ░██████ ░██    ░██   ░█████  ░██    ░██ 
+░██     ░██ ░██   ░██           ░██    ░██       ░██ ░██    ░██ 
+ ░██   ░██  ░██    ░██          ░██   ░███ ░██   ░██ ░██    ░██ 
+  ░██████   ░██     ░██          ░█████░██  ░██████  ░██    ░██ 
+       ░██                             ░██                      
+        ░██                      ░███████                       
+                                                                
+
 """ + Fore.GREEN + Style.BRIGHT + "          For Red Teamers v.1\n")
 
 def shorten_url_with_bitly(long_url, token):
