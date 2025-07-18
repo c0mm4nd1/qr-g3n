00f2d13465ccc6205e71e5e056f83d9479c5a5ac
c0mm4nd1 <111929298+c0mm4nd1@users.noreply.github.com>
2025-07-18 10:35:15 -0500

    install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 echo "📦 Creating virtual environment..."
 python3 -m venv venv
-
 echo "✅ Activating virtual environment..."
 source venv/bin/activate
-
 echo "⬇️ Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
-
 echo "🎉 Installation complete."
 
